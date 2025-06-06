3a151b97a5a3c67bf0ce5fe386a6b48659f11e7d
John Lee <jahndahp@gmail.com>
2025-06-05 19:07:44 -0700

    Latest Commit

--- Run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-IF NOT EXIST "vcpkg\bootstrap-vcpkg.bat" (
-    git submodule update --init --recursive
-)
-
-CALL vcpkg\bootstrap-vcpkg.bat
-
-cmake --preset windows-default
-cmake --build --preset windows-default
