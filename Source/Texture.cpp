e17a5baee80f97a9b75bed50d89cb4c64d84aecf
John Lee <jahndahp@gmail.com>
2025-06-19 17:30:03 -0700

    Added Mipmap Texture Changes

--- Source/Texture.cpp
+++ Source/Texture.cpp
@@ -11,8 +11,8 @@ Texture::Texture(const char* path)
 	glActiveTexture(GL_TEXTURE0);
 
 	bind();
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, loadedImage);
